c0b403b045be553276d45f655b229c83f7bf0de4
NicolasNemeth <52066435+NicolasNemeth@users.noreply.github.com>
2020-11-30 11:12:35 +0100

    Added points

---
+++ PacBoy/Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Point"))
+        {
+            Debug.Log("Collided");
+
+            collision.gameObject.SetActive(false);
+
+            //Add Points to Score
+            //Check if all points have been collected
+        }
+    }
+}
